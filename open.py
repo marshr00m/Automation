279b58dd2c06f8ecf66d4290c3e825ca48ffd727
marshr00m <marssub2300000@gmail.com>
2021-05-06 05:31:44 +0900

    Auto quit function added

--- open.py
+++ open.py
@@ -1,33 +1,42 @@
+import config
+import datetime
+import sys
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-import time, datetime, sys, config
 
 # Import arguments from config.py file
 email = config.EMAIL_ADDRESS
 passwd = config.PASSWORD
 url = config.URL
-#subject = config.SUBJECT_NAME
+# subject = config.SUBJECT_NAME
 
-#Import arguments from argv
+# Import arguments from argv
 subject = sys.argv[1]
 
 ########################################################################
+# Configuration
 ########################################################################
-####                        Configuration                           ####
-########################################################################
-########################################################################
+
 
 # Class session length (minute)
 length = 92
 
+# If Meet people number is 1, count will decrease 1 by a minute.
+count_to_exit = 10
+
+# People number detection and system message interval (seconds)
+message_interval = 60
+
 # Define locations
-driver_location = ".\chromedriver.exe"
+driver_location = ".\\chromedriver.exe"
 profile_path = "C:\\ChromeProfiles\\Automation"
 profile_dir = "Default"
 
 # Google login page URL
-login_url = "https://accounts.google.com/signin/v2/identifier?hl=ja&passive=true&continue=https%3A%2F%2Fwww.google.com%2F%3Fhl%3Dja&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+login_url = "https://accounts.google.com/signin/v2/identifier?hl=ja&passive=true&continue=https%3A%2F%2Fwww.google" \
+            ".com%2F%3Fhl%3Dja&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin "
 
 # Chrome configurations
 # 1 - Profile
@@ -38,7 +47,7 @@ opt.add_argument("--profile-directory=" + profile_dir)
 
 # 2 - Permission
 # Pass the argument 1 to allow and 2 to block
-opt.add_experimental_option("prefs", { \
+opt.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 2,
     "profile.default_content_setting_values.media_stream_camera": 2,
     "profile.default_content_setting_values.geolocation": 2,
@@ -58,7 +67,8 @@ def classroom(sub):
     driver.get(url)
     sysmessage("Searching the classrooms for [ " + sub + " ]...")
     driver.find_element_by_partial_link_text(sub).click()
-    sysmessage("Found a classroom named [ " + driver.find_element_by_partial_link_text(sub).text.replace("\n", " ") + " ].")
+    sysmessage(
+        "Found a classroom named [ " + driver.find_element_by_partial_link_text(sub).text.replace("\n", " ") + " ].")
 
     meet_url = driver.find_element_by_partial_link_text("meet.google.com").text
     sysmessage()
@@ -106,34 +116,34 @@ def sysmessage(text_message=""):
 session_url = classroom(subject)
 meetJoin(session_url)
 
-
 # Timer
 joined_time = time.time()
 sec_time = time.time() - joined_time
 
-time.sleep(2)
-people = driver.find_element_by_xpath(
-    '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[1]/div[3]/div/div[2]/div[1]/span/span/div/div/span[2]'
-).text
-print(people + "nin test")
-
 while True:
     time.sleep(1)
     ela_time = time.time() - joined_time
-    if ela_time > sec_time + 10:
-        min = ela_time / 60
-        people = driver.find_element_by_xpath(
+
+    if ela_time > sec_time + message_interval:
+        mins = ela_time / 60
+        person_count = driver.find_element_by_xpath(
             '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[1]/div[3]/div/div[2]/div[1]/span/span/div/div/span[2]'
-            ).text
+        ).text
         sysmessage()
-        print('{:.0f}'.format(min) + "/" + str(length) + " min passed, " + str(round(length - min)) + " min to quit. " + str(people) + " people in the meet session.")
+        print('{:.0f}'.format(mins) + "/" + str(length) + " min passed. People count: " + person_count + ".", end="")
         sec_time = ela_time
-        if people is not "1":
+        if int(person_count) > 3:
             pass
         else:
-            sysmessage("Quitting process...")
-            driver.quit()
-            break
+            count_to_exit -= 1
+            print(" No Meet activities. Exit in " + str(count_to_exit + 1) + ".", end="")
+        print("")
+
+    if count_to_exit == 0:
+        sysmessage("Quitting process...")
+        driver.quit()
+        break
+
     if ela_time > length * 60:
         sysmessage()
         print(str(length) + " min timer reached")
