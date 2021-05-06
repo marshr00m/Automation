56aaad943511aa0059c252e0e8a431eb9e6e4472
marshr00m <marssub2300000@gmail.com>
2021-05-06 18:18:41 +0900

    changed log system to logging.

--- open.py
+++ open.py
@@ -1,10 +1,13 @@
-import config
 import datetime
+import logging
 import sys
 import time
+import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+import config
 
 # Import arguments from config.py file
 email = config.EMAIL_ADDRESS
@@ -15,10 +18,10 @@ url = config.URL
 # Import arguments from argv
 subject = sys.argv[1]
 
+
 ########################################################################
 # Configuration
 ########################################################################
-
 
 # Class session length (minute)
 length = 92
@@ -29,17 +32,21 @@ count_to_exit = 10
 # People number detection and system message interval (seconds)
 message_interval = 60
 
-# Define locations
-driver_location = ".\\chromedriver.exe"
-profile_path = "C:\\ChromeProfiles\\Automation"
-profile_dir = "Default"
+# Logfile location
+logfileDir = './logs/'
+
 
 # Google login page URL
 login_url = "https://accounts.google.com/signin/v2/identifier?hl=ja&passive=true&continue=https%3A%2F%2Fwww.google" \
             ".com%2F%3Fhl%3Dja&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin "
 
+
 # Chrome configurations
 # 1 - Profile
+driver_location = ".\\chromedriver.exe"
+profile_path = "C:\\ChromeProfiles\\Automation"
+profile_dir = "Default"
+
 opt = Options()
 opt.add_argument("start-maximized")
 opt.add_argument("--user-data-dir=" + profile_path)
@@ -56,6 +63,24 @@ opt.add_experimental_option("prefs", {
 ########################################################################
 ########################################################################
 
+# Create directory if not exists
+if not os.path.exists(logfileDir):
+    os.makedirs(logfileDir)
+
+# Logger
+logger = logging.getLogger('AutoMeetJoin')
+logger.setLevel(20)
+
+sh = logging.StreamHandler()
+fh = logging.FileHandler('logs/{:%Y-%m-%d-%H-%M-%S}.log'.format(datetime.datetime.now()))
+logger.addHandler(sh)
+logger.addHandler(fh)
+
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+fh.setFormatter(formatter)
+sh.setFormatter(formatter)
+
+
 # Launch chrome
 driver = webdriver.Chrome(options=opt)
 driver.implicitly_wait(30)
@@ -63,16 +88,15 @@ driver.implicitly_wait(30)
 
 # Move to objective website
 def classroom(sub):
-    sysmessage("Opening Google Classroom...")
+    logger.info("Opening Google Classroom...")
     driver.get(url)
-    sysmessage("Searching the classrooms for [ " + sub + " ]...")
+    logger.info("Searching the classrooms for [ " + sub + " ]...")
     driver.find_element_by_partial_link_text(sub).click()
-    sysmessage(
+    logger.info(
         "Found a classroom named [ " + driver.find_element_by_partial_link_text(sub).text.replace("\n", " ") + " ].")
 
     meet_url = driver.find_element_by_partial_link_text("meet.google.com").text
-    sysmessage()
-    print("Target Google Meet URL: " + meet_url)
+    logger.info("Target Google Meet URL: " + meet_url)
     return meet_url
 
 
@@ -81,17 +105,16 @@ def meetJoin(target_url):
     driver.execute_script("window.open('about:blank','second_tab');")
     driver.switch_to.window("second_tab")
     driver.get(target_url)
-    # print(driver.find_element_by_xpath("/html/body").text)
     time.sleep(3)
     driver.find_element_by_xpath(
         '//*[@id="yDmH0d"]/div[3]/div/div[2]/div[3]/div/span/span').click()
     time.sleep(1)
     driver.find_element_by_xpath(
-        '//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/span/span'
+        '//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div['
+        '1]/span/span'
     ).click()
     end_date = datetime.datetime.now() + datetime.timedelta(minutes=length)
-    sysmessage()
-    print("Joined the class. End time will be " + end_date.strftime("%Y/%m/%d %H:%M:%S"))
+    logger.info("Joined the class. End time will be " + end_date.strftime("%Y/%m/%d %H:%M:%S"))
 
 
 # If you have to login to the Google Account
@@ -105,12 +128,6 @@ def login():
     ).send_keys(passwd + "\n")
 
 
-def sysmessage(text_message=""):
-    print("[" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "] ", end="")
-    if text_message != "":
-        print(text_message)
-
-
 # Execute
 # login()
 session_url = classroom(subject)
@@ -119,6 +136,8 @@ meetJoin(session_url)
 # Timer
 joined_time = time.time()
 sec_time = time.time() - joined_time
+
+countFlag = count_to_exit
 
 while True:
     time.sleep(1)
@@ -129,25 +148,24 @@ while True:
         person_count = driver.find_element_by_xpath(
             '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[1]/div[3]/div/div[2]/div[1]/span/span/div/div/span[2]'
         ).text
-        sysmessage()
-        print('{:.0f}'.format(mins) + "/" + str(length) + " min passed. People count: " + person_count + ".", end="")
+        logger.info('{:.0f}'.format(mins) + "/" + str(length) + " min passed. People count: " + person_count + ".")
         sec_time = ela_time
         if int(person_count) > 3:
             pass
+            if countFlag != count_to_exit:
+                countFlag = count_to_exit
         else:
             count_to_exit -= 1
-            print(" No Meet activities. Exit in " + str(count_to_exit + 1) + ".", end="")
-        print("")
+            logger.warning("No Meet activities. Exit in " + str(count_to_exit + 1) + ".")
 
     if count_to_exit == 0:
-        sysmessage("Quitting process...")
+        logger.info("Quitting process...")
         driver.quit()
         break
 
     if ela_time > length * 60:
-        sysmessage()
-        print(str(length) + " min timer reached")
-        sysmessage("Quitting process...")
+        logger.info(str(length) + " min timer reached")
+        logger.info("Quitting process...")
         driver.find_element_by_xpath(
             '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[9]/div[2]/div[2]/div'
         ).click()
